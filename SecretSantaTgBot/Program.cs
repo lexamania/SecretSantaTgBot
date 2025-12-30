9faa3ea63acc58341cf7469f505e3db0425fa5fb
lexamania <torbanowmain@gmail.com>
2025-12-30 13:43:29 +0200

    remove on bot start notify

--- SecretSantaTgBot/Program.cs
+++ SecretSantaTgBot/Program.cs
@@ -1,12 +1,10 @@
 ﻿using SecretSantaTgBot;
-using SecretSantaTgBot.Models;
 using SecretSantaTgBot.Services;
 using SecretSantaTgBot.Storage;
 
 using Telegram.Bot;
 
 var botToken = EnvVariables.BotToken;
-var appArgs = new AppArgs(args);
 
 using var cts = new CancellationTokenSource();
 using var db = new SantaDatabase();
@@ -23,23 +21,11 @@ bot.OnError += errorBroker.OnError;
 bot.OnMessage += msgBroker.OnMessage;
 bot.OnUpdate += queryBroker.OnUpdate;
 
-if (!appArgs.IsSilentStart)
-{
-    var startUsers = db.Users.FindAll().Select(x => x.Id).ToArray();
-    await notifyService.NotifyEveryone(startUsers, "Бот знову активний!", true);
-}
-
 var me = await bot.GetMe();
 EnvVariables.BotName = me.Username!;
 
 Console.WriteLine($"@{me.Username} is running... Send STOP to terminate");
 
 while (Console.ReadLine() != "STOP") ;
-
-if (!appArgs.IsSilentStop)
-{
-    var endUsers = db.Users.FindAll().Select(x => x.Id).ToArray();
-    await notifyService.NotifyEveryone(endUsers, "Бот відключено!", true);
-}
 
 cts.Cancel(); // stop the bot
